d7817b5968a29965aaca643e0aeeeceaf83c7e10
jmsdrake <jms.drake@gmail.com>
2018-09-12 13:53:53 +0200

    Allow failover servers with 0 wieght

--- src/balance/weight.go
+++ src/balance/weight.go
@@ -28,22 +28,34 @@ func (b *WeightBalancer) Elect(context core.Context, backends []*core.Backend) (
 
 	totalWeight := 0
 	for _, backend := range backends {
-		if backend.Weight <= 0 {
-			return nil, errors.New("Invalid backend weight 0")
+		if backend.Weight < 0 {
+			return nil, errors.New("Invalid backend weight <0")
 		}
 		totalWeight += backend.Weight
 	}
+	if totalWeight == 0 {
+		r1 := rand.Intn(len(backends)-1)
+		pos := 0
 
-	r := rand.Intn(totalWeight)
-	pos := 0
-
-	for _, backend := range backends {
-		pos += backend.Weight
-		if r >= pos {
-			continue
+		for _, backend := range backends {
+			if r1 <= pos {
+				pos += 1
+				continue
+			}
+			return backend, nil
 		}
-		return backend, nil
 	}
+	else {
+		r2 := rand.Intn(totalWeight)
+		pos := 0
 
+		for _, backend := range backends {
+			pos += backend.Weight
+			if r2 >= pos {
+				continue
+			}
+			return backend, nil
+		}
+	}
 	return nil, errors.New("Can't elect backend")
 }
