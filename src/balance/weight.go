aa046e2120f23beab61617b5d960e8a2edbc34cb
James Drake <james.drake@ericsson.com>
2018-09-12 16:29:21 +0200

    Rustam update to weight

--- src/balance/weight.go
+++ src/balance/weight.go
@@ -1,4 +1,4 @@
-/**
+	/**
  * weight.go - weight balance impl
  *
  * @author Yaroslav Pogrebnyak <yyyaroslav@gmail.com>
@@ -10,6 +10,7 @@ import (
 	"../core"
 	"errors"
 	"math/rand"
+	"../logging"
 )
 
 /**
@@ -27,24 +28,38 @@ func (b *WeightBalancer) Elect(context core.Context, backends []*core.Backend) (
 	}
 
 	totalWeight := 0
+	log := logging.For("elect/weight")
+
 	for _, backend := range backends {
 		if backend.Weight < 0 {
 			return nil, errors.New("Invalid backend weight <0")
 		}
 		totalWeight += backend.Weight
 	}
-	if totalWeight == 0 {
-		r1 := rand.Intn(len(backends)-1)
-		pos := 0
 
-		for _, backend := range backends {
-			if r1 <= pos {
-				pos += 1
-				continue
+	if totalWeight == 0 {
+		if len(backends) == 1 {
+			for _, backend := range backends {
+				return backend, nil
 			}
-			return backend, nil
+		} else {
+			r1 := rand.Intn(len(backends)-1)
+			pos := 0
+		
+			for _, backend := range backends {
+
+				if r1 >= pos {
+					return backend, nil
+				}
+				pos += 1
+
+			}
+
 		}
+
 	} else {
+		log.Warn("total weight is:", totalWeight)
+
 		r2 := rand.Intn(totalWeight)
 		pos := 0
 
